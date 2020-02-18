cfbb26eff8cc0c7ff4640a6d478a717448d3a873
Daniel Butterfield <daniel.butterfield@komododigital.co.uk>
2020-02-18 12:24:34 +0000

    Replace textarea with slate

--- src/components/TextInputBox/TextInputBox.js
+++ src/components/TextInputBox/TextInputBox.js
@@ -1,27 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from 'react';
+import { createEditor, Node } from 'slate';
+import { Slate, Editable, withReact } from 'slate-react';
+import { withHistory } from 'slate-history';
 
 function TextInputBox({ messagesApi, user }) {
-
-  const [newMessage, setNewMessage] = useState('');
+  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
+  const [newMessage, setNewMessage] = useState([ { children: [ { text: '' } ] } ]);
 
   const onCreate = (e) => {
     e.preventDefault();
-    messagesApi.add({ message: newMessage, createdAt: new Date(), userId: user });
-    setNewMessage('')
-  };
+    const text = parseText(newMessage);
+
+    // Add the message to the database
+    messagesApi.add({ message: text, createdAt: new Date(), userId: user.uid });
+
+    // ... then we can reset state
+    setNewMessage([ { children: [ { text: '' } ] } ]);
+  }
+
+  const parseText = (nodes) => {
+    return nodes.map(n => Node.string(n)).join('\n')
+  }
 
   return (
     <div className={`textInput`}>
-      <textarea
-        value={newMessage}
-        onChange={e => {
-          setNewMessage(e.target.value)
-
-          if (e.target.value.slice(-1) === "\n") {
-            onCreate(e);
-          }
-        }}
-      ></textarea>
+      <Slate editor={editor} value={newMessage} onChange={value => {
+          setNewMessage(value);
+        }}>
+        <Editable placeholder="Enter some plain text..." />
+      </Slate>
       <button onClick={onCreate}>Send</button>
     </div>
   );
