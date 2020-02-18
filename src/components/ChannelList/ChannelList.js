3089ebcbbe1caf687964d785d6812bed241bbf24
Daniel Butterfield <daniel.butterfield@komododigital.co.uk>
2020-02-18 13:56:34 +0000

    Add channel list component

---
+++ src/components/ChannelList/ChannelList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ChannelList({ messages, user }) {
+
+  return (
+    <div className={`channel`}>
+        <p>You could add channels list</p>
+    </div>
+  );
+}
+
+export default ChannelList;
