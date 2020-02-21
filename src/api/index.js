aec722578f7d73af7a70bdce8156b5a38f7508c7
Ian Outterside <ian@komododigital.co.uk>
2020-02-21 15:42:27 +0000

    Remove api keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyDGdtOYgnt5tHlSu4iWTH_w43q6aZp6W-M",
-  authDomain: "slackover-f77c9.firebaseapp.com",
-  databaseURL: "https://slackover-f77c9.firebaseio.com",
-  projectId: "slackover-f77c9",
-  storageBucket: "slackover-f77c9.appspot.com",
-  messagingSenderId: "50629004745",
-  appId: "1:50629004745:web:14bf9209b9995598a8ac18"
+  apiKey: "<your key here>",
+  authDomain: "<your key here>",
+  databaseURL: "<your key here>",
+  projectId: "<your key here>",
+  storageBucket: "<your key here>",
+  messagingSenderId: "<your key here>",
+  appId: "<your key here>8"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
